46e8e4307ef11542595c0369c13c1b8a0f5472dc
laskin94 <tema-hoodoznik@mail.ru>
2014-11-26 19:48:47 +0300

    begining

---
+++ core/src/com/mousesvscats/game/GameMousesVSCats.java
@@ -0,0 +1,25 @@
+package com.mousesvscats.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameMousesVSCats extends Game {
+	SpriteBatch batch;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 1, 1, 1); //цвет очищения
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //само очищение
+		batch.begin();
+		batch.end();
+	}
+}
---
+++ desktop/src/com/mousesvscats/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.mousesvscats.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mousesvscats.game.GameMousesVSCats;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.height = 600;
+        config.width = 800;
+		new LwjglApplication(new GameMousesVSCats(), config);
+	}
+}
