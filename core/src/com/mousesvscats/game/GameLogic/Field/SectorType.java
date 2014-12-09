322f9fa98142bac75b6ce0224ee7249c03766ecc
laskin94 <tema-hoodoznik@mail.ru>
2014-12-09 21:00:54 +0300

    made Labyrinth and Sector

---
+++ core/src/com/mousesvscats/game/GameLogic/Field/SectorType.java
@@ -0,0 +1,11 @@
+package com.mousesvscats.game.GameLogic.Field;
+
+/**
+ * Created by User on 09.12.2014.
+ */
+public enum SectorType {
+    EMPTY,          //свободная клетка
+    WALL,           //стена
+    CLOSED_DOOR,    //закрытая дверь
+    OPENED_DOOR;    //открытая дверь
+}
